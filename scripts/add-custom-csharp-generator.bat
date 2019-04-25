a96b959f2890ecfab91e5a0896a3e43293fb4500
Dragos Rogojan <drogojan@salesforce.com>
2019-04-25 14:57:59 +0300

    Updated scripts to use swagger-codegen-cli-2.4.4

--- scripts/add-custom-csharp-generator.bat
+++ scripts/add-custom-csharp-generator.bat
@@ -1,1 +1,1 @@
-java -cp ../generators/csharp/target/csharp-swagger-codegen-1.0.0.jar;../cli/swagger-codegen-cli.jar io.swagger.codegen.SwaggerCodegen
+java -cp ../generators/csharp/target/csharp-swagger-codegen-1.0.0.jar;../cli/swagger-codegen-cli-2.4.4.jar io.swagger.codegen.SwaggerCodegen
