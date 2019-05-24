06f5b597ac3d00d6de73579688d06417c4ffb9c1
Dragos Rogojan <drogojan@salesforce.com>
2019-05-24 18:00:52 +0300

    Automation pipeline script update

--- scripts/create-nuget-package.bat
+++ scripts/create-nuget-package.bat
@@ -2,3 +2,4 @@ pushd "..\openapi-2.0\generated\src\Salesforce.MarketingCloud"
 
 ..\..\..\..\build-artifacts\nuget.exe pack Salesforce.MarketingCloud.nuspec -Properties Configuration=Release -OutputDirectory "..\..\..\..\build-artifacts"
 
+popd
