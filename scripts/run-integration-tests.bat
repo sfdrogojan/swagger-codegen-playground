e1fc9ec36682533c51f2f3fb3ccfad0715642096
Dragos Rogojan <drogojan@salesforce.com>
2019-05-28 17:13:24 +0300

    Automation pipeline script update

--- scripts/run-integration-tests.bat
+++ scripts/run-integration-tests.bat
@@ -1,7 +1,6 @@
 :: Run Integration Tests
 
 dotnet test ..\openapi-2.0\generated\src\Salesforce.MarketingCloud.Test\Salesforce.MarketingCloud.Test.csproj ^
-    --results-directory:..\..\..\..\build-artifacts ^
-    --logger:trx
+    --logger:console
 
 if %errorlevel% neq 0 exit %errorlevel%
--- scripts/run-unit-tests.bat
+++ scripts/run-unit-tests.bat
@@ -1,8 +1,7 @@
 :: Run Unit Tests
 
 dotnet test ..\openapi-2.0\generated\src\Salesforce.MarketingCloud.UnitTests\Salesforce.MarketingCloud.UnitTests.csproj ^
-    --results-directory:..\..\..\..\build-artifacts ^
-    --logger:trx ^
+    --logger:console ^
     --verbosity:minimal
 
 if %errorlevel% neq 0 exit %errorlevel%
