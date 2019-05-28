8605cc9c7770985c88a2f7ad277bb9467c270712
Dragos Rogojan <drogojan@salesforce.com>
2019-05-28 16:59:19 +0300

    Updates

--- scripts/run-integration-tests.bat
+++ scripts/run-integration-tests.bat
@@ -1,10 +1,7 @@
 :: Run Integration Tests
 
-SET NUNITCONSOLEPATH=..\openapi-2.0\generated\packages\NUnit.ConsoleRunner.3.10.0\tools
-
-%NUNITCONSOLEPATH%\nunit3-console.exe ^
-    ..\openapi-2.0\generated\src\Salesforce.MarketingCloud.Test\bin\Release\Salesforce.MarketingCloud.Test.dll ^
-    --result=IntegrationTestsResult.xml ^
-    --work=..\build-artifacts
+dotnet test ..\openapi-2.0\generated\src\Salesforce.MarketingCloud.Test\Salesforce.MarketingCloud.Test.csproj ^
+    --results-directory:..\..\..\..\build-artifacts ^
+    --logger:trx
 
 if %errorlevel% neq 0 exit %errorlevel%
--- scripts/run-unit-tests.bat
+++ scripts/run-unit-tests.bat
@@ -1,10 +1,8 @@
 :: Run Unit Tests
 
-SET NUNITCONSOLEPATH=..\openapi-2.0\generated\packages\NUnit.ConsoleRunner.3.10.0\tools
-
-%NUNITCONSOLEPATH%\nunit3-console.exe ^
-    ..\openapi-2.0\generated\src\Salesforce.MarketingCloud.UnitTests\bin\Release\Salesforce.MarketingCloud.UnitTests.dll ^
-    --result=UnitTestsResult.xml ^
-    --work=..\build-artifacts
+dotnet test ..\openapi-2.0\generated\src\Salesforce.MarketingCloud.UnitTests\Salesforce.MarketingCloud.UnitTests.csproj ^
+    --results-directory:..\..\..\..\build-artifacts ^
+    --logger:trx ^
+    --verbosity:minimal
 
 if %errorlevel% neq 0 exit %errorlevel%
