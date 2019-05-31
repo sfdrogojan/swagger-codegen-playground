d31e9035507608b3d99f52cc7193dd9f4510aec9
Dragos Rogojan <dragos.rogojan@gmail.com>
2019-06-01 00:59:15 +0300

    Added linux-amd64 hub

--- scripts/create-pull-request-linux.sh
+++ scripts/create-pull-request-linux.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-cd "../cli/hub-linux-arm64-2.11.2"
+ROOT_DIRECTORY=$(dirname $(dirname $(readlink -f "$0")))
+HUB_PATH="$ROOT_DIRECTORY/cli/hub-linux-amd64-2.11.2"
+echo $HUB_PATH
+export PATH=$HUB_PATH:$PATH
+
+hub version
 
 hub pull-request -m "Automation pipeline update"
-
-cd "../../scripts"
 
 exit $?
